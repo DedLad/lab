de2d24be39fa1f1449943b3b74f94bfbdb3d1acd
DedLad <adheshathrey2004.com>
2023-11-06 22:02:05 +0530

    updates

--- 15.py
+++ 15.py
@@ -1,9 +1,4 @@
-# # Get input from the user
-# input_string = input("Enter a string: ")
 
-# # Find unique substrings and print them in lexicographical order
-# substrings = sorted({input_string[i:j] for i in range(len(input_string)) for j in range(i + 1, len(input_string) + 1)})
-# print("Unique substrings in lexicographical order:", substrings)
 
 a= input('enter string ')
 s=set()
