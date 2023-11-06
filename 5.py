b33adf7a035efb8f273ba43f08458c29b15dfb71
DedLad <adheshathrey2004.com>
2023-11-06 20:16:46 +0530

    fixed program 5

--- 5.py
+++ 5.py
@@ -1,9 +1,11 @@
 l = []
 for i in range (1,int(input('number of elements in list '))+1):
     l.append(int(input(f'enter value {i} ')))
-s = int(input('enter count for element to include '))
-for i in l:
-    if l.count(i)!=s:
-        l.remove(i)
-
 print(l)
+s = int(input('enter count for element to include '))
+r=[]
+for i in l:
+    if l.count(i)==s:
+        r.append(i)
+
+print(r)
